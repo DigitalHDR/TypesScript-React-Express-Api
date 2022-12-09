4bc32dd204c1fbfb65876f4894f2814732ffd152
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-09 13:50:20 -0300

    validacao de parametros opcional

---
+++ 3_TIPOS/validacao_de_parametros_opcional/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*  | melhor forma de validar parametros com typeScript |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// function advanceGreeting(firstName: string, lastName?: string) {
+//   if (lastName !== undefined) {
+//     //diferente de undefined é por que tem lastName
+//     return `Olá, ${firstName} ${lastName} tudo bem!`
+//   }
+//   return `Olá, ${firstName} tudo bem!`
+// }
+// console.log(advanceGreeting('lucas'))
+// console.log(advanceGreeting('lucas', 'fernando'))
