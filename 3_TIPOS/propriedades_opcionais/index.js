5bdd7c45a53c794f231acc94a339521a0e77965b
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-09 13:36:23 -0300

    propriedades opcionais com '?'

---
+++ 3_TIPOS/propriedades_opcionais/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// function showNumber(a: number, b: number, c?: number) {
+//   console.log('a:' + a)
+//   console.log('b:' + b)
+//   if (c) {
+//     // lógica para mostrar o 'c' no chamada da 2° função se tiver
+//     // 'c' mostrar se nao tiver nao mostrar (para não mostrar undefined)
+//     console.log('c:' + c)
+//   }
+//   console.log('=======')
+// }
+// showNumber(1, 2, 3) //* o normal vai funcionar com os 3 parametros
+// showNumber(4, 5) //* o parametro c é opcional, pode ou não ter (por isso tem o ?)
+// // showNumber(1) //! se tiver apenas 1 paremetro, da erro, pois espera no minimo 2
