5f75a92c6f200aa40613b461ac20df0794f1cf95
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-23 13:00:45 -0300

    Bigint

---
+++ 3_TIPOS/bigInt/index.ts
@@ -0,0 +1,23 @@
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*                    | Bigint |
+//* com o tipo bigint podemos declarar números com valores muito altos
+//* podemos utilizar a notação literal, exemplo: 100n
+//* para este recurso precisamos mudar a configuração do TS, para versão
+//* mínima de ES2020
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+let n: bigint
+
+// n = 1 //! da erro pois não satisfaz o bigint
+
+n = 1000n
+//! tem que mudar a config do TS para ES2020, pois só ele(eu acho) funciona Bigint 
+
+console.log(n)
+//!1000n
+
+console.log(typeof n)
+//! bigint
+
+console.log(n + 100n)
+//! 1100n
