d74e9ca6ae0cf12835499cae036709b462c6038e
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-27 15:33:25 -0300

    checando se valor existe

---
+++ 4_NARROWING/checando_se_valor_existe/index.ts
@@ -0,0 +1,35 @@
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*              | Checando se valor existe |
+//* em javascript podemos colocar uma variavel em um if, e se
+//* houver algum valor recebemos um true
+//* quando não há valor um false
+//* desta maneira conseguimos realizar o narrowing tambem
+//* , é uma outra estratégia bem utilizada
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function operations(arr: number[], operation?: string | undefined) {
+  if (operation) {
+
+    if (operation === 'sum') {
+      const sum = arr.reduce((i, total) => i + total)
+      console.log(sum)
+
+    } else if (operation === 'multiply') {
+      const multiply = arr.reduce((i, total) => i * total)
+      console.log(multiply)
+
+    }
+    
+  } else {
+    console.log('Por favor, defina uma operação')
+  }
+}
+
+operations([1, 2, 3])
+//!Por favor, defina uma operação
+
+operations([1, 2, 3], 'sum')
+//! 6
+
+operations([2, 4, 8], 'multiply')
+//! 64
