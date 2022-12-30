e3ab4f83321f9eabd4f8ce06db91ecfab1ef2484
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-12-29 22:15:52 -0300

    funcao que nao retornam nada

---
+++ 5_FUNCAO_COM_TYPESCRIPT/funcao_que_nao_retornam_nada/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*              | funcao que nao retornam nada |
+//* podemos ter funções que não retornam valores
+//* qual seria o tipo de dado indicado para essa situacao
+//* neste caso utilizamos o void
+//* ele vai dizer ao ts que não temos um valor de retorno
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function withowtReturn() {
+    console.log(`esta função nao tem retorno`);
+    // return '1'
+    // return 1
+    //? não aceita return seja qual for o tipo
+}
+withowtReturn();
+//! esta função nao tem retorno
