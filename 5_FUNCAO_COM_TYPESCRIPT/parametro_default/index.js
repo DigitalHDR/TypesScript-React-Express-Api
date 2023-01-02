6523548194bceb2ef8a6e52ab11436787b65f5a9
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-02 16:58:19 -0300

    revisão em parametros por default

--- 5_FUNCAO_COM_TYPESCRIPT/parametro_default/index.js
+++ 5_FUNCAO_COM_TYPESCRIPT/parametro_default/index.js
@@ -4,13 +4,14 @@
 //* os parâmetros default são os que ja possuem um valor definido
 //* se não passarmos para a função, é utilizado esse valor
 //* para este recurso, a aplicação em ts é igual js
+//? precisa estar sempre por ultimo na lista de argumentos
 //* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// function somaDefault(n: number, m = 10): number {
-//     return n + m
-// }
-// console.log(somaDefault(2))
-// //! 12
-// console.log(somaDefault(2, 5))
-// //! 7
-// //? o segundo valor fica como default se vc colocar sera ele, se nao ficara
-// //? oque está no parmetro
+function somaDefault(n, m = 5) {
+    return n + m;
+}
+console.log(somaDefault(2));
+//! 12
+console.log(somaDefault(2, 5));
+//! 7
+//? o segundo valor fica como default se vc colocar sera ele, se nao ficara
+//? oque está no parmetro
--- 5_FUNCAO_COM_TYPESCRIPT/parametro_default/index.ts
+++ 5_FUNCAO_COM_TYPESCRIPT/parametro_default/index.ts
@@ -3,17 +3,19 @@
 //* os parâmetros default são os que ja possuem um valor definido
 //* se não passarmos para a função, é utilizado esse valor
 //* para este recurso, a aplicação em ts é igual js
+
+//? precisa estar sempre por ultimo na lista de argumentos
 //* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-// function somaDefault(n: number, m = 10): number {
-//     return n + m
-// }
+function somaDefault(n: number, m = 5): number {
+    return n + m
+}
 
-// console.log(somaDefault(2))
-// //! 12
+console.log(somaDefault(2))
+//! 12
 
-// console.log(somaDefault(2, 5))
-// //! 7
+console.log(somaDefault(2, 5))
+//! 7
 
-// //? o segundo valor fica como default se vc colocar sera ele, se nao ficara
-// //? oque está no parmetro
+//? o segundo valor fica como default se vc colocar sera ele, se nao ficara
+//? oque está no parmetro
