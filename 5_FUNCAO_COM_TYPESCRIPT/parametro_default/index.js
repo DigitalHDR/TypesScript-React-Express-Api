e891c6e7199b6a11263f6c57b8dce66170e0c273
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-02 15:25:11 -0300

    Parâmetro default

---
+++ 5_FUNCAO_COM_TYPESCRIPT/parametro_default/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*              | Parâmetro default |
+//* os parâmetros default são os que ja possuem um valor definido
+//* se não passarmos para a função, é utilizado esse valor
+//* para este recurso, a aplicação em ts é igual js
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function somaDefault(n, m = 10) {
+    return n + m;
+}
+console.log(somaDefault(2));
+//! 12
+console.log(somaDefault(2, 5));
+//! 7
+//? o segundo valor fica como default se vc colocar sera ele, se nao ficara
+//? oque está no parmetro
