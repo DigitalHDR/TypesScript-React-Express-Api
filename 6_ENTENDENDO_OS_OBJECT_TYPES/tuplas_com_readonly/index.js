d5815c51ff0ae39b960dc1cf9559ccb755e1f30e
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-06 14:18:04 -0300

    Tuplas com readonly

---
+++ 6_ENTENDENDO_OS_OBJECT_TYPES/tuplas_com_readonly/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*          | Tuplas com readonly |
+//* podemos criar tuplas com a propriedade de readonly
+//* é um tipo de dado super restrito pois
+//* limita quantos itens teremos, qual o tipo de cada um e
+//* também não são modificáveis
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// function showNumbers(numbers: readonly [number, number]) {
+// //   numbers[0] = 10 //! da erro, pois nao pode mudar readonly
+//   console.log(numbers[0])
+//   console.log(numbers[1])
+// }
+// showNumbers([1, 2])
+// // 1
+// // 2
