07b1d58f126aabfa4b8c88c84cbe2d251a46803e
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-06 14:03:57 -0300

    Tuplas

---
+++ 6_ENTENDENDO_OS_OBJECT_TYPES/tuplas/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*          | Tuplas |
+//* é um tipo de array, porem definimos a quantidade e o tipo
+//* de elementos, basicamente criamos um novo type, nele inserimos
+//* um array com os tipo necessários
+//* cada tipo conta também com um elemento
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+const myNumbersArray = [1, 2, 3, 4, 5];
+const anotherUser = ['lucas', 30];
+console.log(anotherUser[0]);
+//! lucas
+anotherUser[0] = 'adima';
+console.log(anotherUser[0]);
+//! adima
---
+++ 6_ENTENDENDO_OS_OBJECT_TYPES/tuplas/index.ts
@@ -0,0 +1,29 @@
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*          | Tuplas |
+//* é um tipo de array, porem definimos a quantidade e o tipo
+//* de elementos, basicamente criamos um novo type, nele inserimos
+//* um array com os tipo necessários
+//* cada tipo conta também com um elemento
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// type fiveNumbers = [number, number, number, number, number]
+
+// const myNumbersArray: fiveNumbers = [1, 2, 3, 4, 5]
+
+// // const myNumbersArray2: fiveNumbers = [1, true, 2, 5] //! da erro
+
+// // const myNumbersArray2: fiveNumbers = [1, 'teste', 2, 5] //! da erro
+
+// //todo =====================================================
+
+// type nameAndAge = [string, number]
+
+// const anotherUser = ['lucas', 30]
+
+// console.log(anotherUser[0])
+// //! lucas
+
+// anotherUser[0] = 'adima'
+
+// console.log(anotherUser[0])
+// //! adima
