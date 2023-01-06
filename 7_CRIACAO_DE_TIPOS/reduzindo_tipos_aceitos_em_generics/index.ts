7c09a566bb5f400d9d816a1f054ba76c8bcd17e8
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-06 15:20:27 -0300

    Reduzindo tipos aceitos em Generics

---
+++ 7_CRIACAO_DE_TIPOS/reduzindo_tipos_aceitos_em_generics/index.ts
@@ -0,0 +1,22 @@
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*      | Reduzindo tipos aceitos em Generics |
+//* as constraints nos ajudam a limitar os tipos aceitos
+//* como em generic podemos ter tipo livres, eles vão filtrar
+//* os tipos aceitos, adicionando organização quando queremos
+//* aproveitar a liberdadde dos generics
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// function showProductName<T extends { name: string }>(obj: T) {
+//   return `O nome do produto é: ${obj.name}`
+// }
+
+// const myObj = { name: 'Porta', cor: 'Branca' }
+// const otherProduct = {name: 'Carro', wheels: 4, engine: 1.0}
+// const thirdObj = {price: 19.99, category: 'Roupa'}
+
+// console.log(showProductName(myObj))
+// //! O nome do produto é: Porta
+
+// console.log(showProductName(otherProduct))
+// //! O nome do produto é: Carro
+
+// // console.log(showProductName(thirdObj)) //! da erro, falta propriedade 'name'
