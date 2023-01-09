d13ade7f30da87dfc81578298b9a11e8aafff061
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-09 18:06:15 -0300

    Keyof type operator

---
+++ 7_CRIACAO_DE_TIPOS/keyof_type_operator/index.ts
@@ -0,0 +1,29 @@
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*          |  Keyof type operator |
+//* com o keyof operator podemos criar um novo tipo
+//* ele aceita dados do tipo objeto, como object literals e arrays
+//* e pode criar o tipo baseado nas chaves do objeto passado com parâmetro
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// type Character = { name: string; age: number; hasDriveLicense: boolean }
+
+// type C = keyof Character
+
+// function showCharName(obj: Character, name: C): string {
+//   return `${obj[name]}`
+// }
+
+// const myChar: Character = {
+//   name: 'Lucas',
+//   age: 30,
+//   hasDriveLicense: true,
+// }
+
+// console.log(showCharName(myChar, 'name'))
+// // Lucas
+
+// console.log(showCharName(myChar, 'age'))
+// // 30
+
+// console.log(showCharName(myChar, 'hasDriveLicense'))
+// // true
