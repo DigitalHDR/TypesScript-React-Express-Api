a44fe1b8f0bb6a1bcb17c122bf7726105520281d
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-09 14:09:22 -0300

    Type parameters

---
+++ 7_CRIACAO_DE_TIPOS/type_parameters/index.js
@@ -0,0 +1,19 @@
+"use strict";
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*          | Type parameters |
+//* é um recurso de generics, utilizado para dizer que algum
+//* parâmetro de uma função, por exemplo, é a chave de um objeto,
+//* que também é parâmetro, desta maneira conseguimos criar uma
+//* ligação entre o tipo genérico e sua chave
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function getSomekey(obj, key) {
+    return `A chave ${[key]} está presente no objeto e tem o valor de ${obj[key]}`;
+}
+const server = {
+    hd: '2TB',
+    ram: '32GB'
+};
+console.log(getSomekey(server, 'ram'));
+//! A chave ram está presente no objeto e tem o valor de 32GB
+console.log(getSomekey(server, 'hd'));
+//! A chave hd está presente no objeto e tem o valor de 2TB
