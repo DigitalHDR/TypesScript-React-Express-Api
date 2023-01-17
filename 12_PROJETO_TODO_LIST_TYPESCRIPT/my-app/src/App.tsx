9c9b00cfac258bba03a15146ea7c9b28a828f240
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-17 11:37:12 -0300

    Criando header e footer

--- 12_PROJETO_TODO_LIST_TYPESCRIPT/my-app/src/App.tsx
+++ 12_PROJETO_TODO_LIST_TYPESCRIPT/my-app/src/App.tsx
@@ -1,9 +1,17 @@
 // import React from 'react';
 // import logo from './logo.svg';
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
-  return <div>Todo react</div>
+  return (
+    <div>
+      <Header />
+      <h1>Conteudo</h1>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
