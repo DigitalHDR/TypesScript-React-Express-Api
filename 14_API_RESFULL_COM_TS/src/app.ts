d2df1a90f5c65488bde877075622cdcc299523a6
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-01-27 13:18:36 -0300

    Integrando o Express

--- 14_API_RESFULL_COM_TS/src/app.ts
+++ 14_API_RESFULL_COM_TS/src/app.ts
@@ -1,1 +1,14 @@
-console.log('deu certo!')
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+//JSON MIDDLEWARE
+app.use(express.json())
+
+// APP PORT
+const port = config.get<number>('port')
+
+app.listen(3000, async() => {
+    console.log(`Aplicação está funcionando na porta: ${port}`)
+})
