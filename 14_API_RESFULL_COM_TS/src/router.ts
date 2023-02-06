832e7d418e223be9afb1e1fe6913e0225f9b4718
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-02-06 14:06:18 -0300

    Middleware de validação de erros

--- 14_API_RESFULL_COM_TS/src/router.ts
+++ 14_API_RESFULL_COM_TS/src/router.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import { createMovie } from './controllers/movieControllers'
 
+// VALIDATIONS
+import { validate } from './middlewere/handleValidation'
+
 const router = Router()
 
-export default router.get(
-  '/test',
-  (req: Request, res: Response, next: NextFunction) => {
+export default router
+  .get('/test', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send('API WORKING!')
-  }
-).post('/movie', createMovie)
+  })
+  .post('/movie', validate, createMovie)
